9ebfa0532b4b2390e0b6ff514836369381078690
Yuta-Haruna <50592688+Yuta-Haruna@users.noreply.github.com>
2023-05-20 13:22:53 +0900

    update: HTMLに経験業務を記載し追加。

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 // トグルボタン
 function toggleText() {
-    var hiddenText = document.getElementById("hidden-text");
-    if (hiddenText.style.display === "none") {
-      hiddenText.style.display = "block"; // 表示する
-    } else {
-      hiddenText.style.display = "none"; // 非表示にする
-    }
+	var hiddenText = document.getElementById("hidden-text");
+	if (hiddenText.style.display === "none") {
+		hiddenText.style.display = "block"; // 表示する
+	} else {
+		hiddenText.style.display = "none"; // 非表示にする
+	}
+}
+
+// トグルボタン
+function toggleText2() {
+	var hiddenText = document.getElementById("hidden-text2");
+	if (hiddenText.style.display === "none") {
+		hiddenText.style.display = "block"; // 表示する
+	} else {
+		hiddenText.style.display = "none"; // 非表示にする
+	}
 }
